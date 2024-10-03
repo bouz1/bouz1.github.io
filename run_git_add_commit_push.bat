501fccda5369ecbd0fbf2a00245425c8a4eb4cf5
bouz1 <bouziani.abdelaziz@gmail.com>
2024-10-03 18:32:02 +0200

    update

--- run_git_add_commit_push.bat
+++ run_git_add_commit_push.bat
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-=======
-cd .\pages\tools\myTools
-py Make_Html_AutoV3.py
-cd ..
-cd ..
-cd ..
->>>>>>> af6ab99f7d883a84b6366f35c47c3d67b2544131
 git add -A
 git commit -m "update"
 git push
